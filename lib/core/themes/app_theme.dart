624e916a4e1f4d81324ace1ff3fe3bb05dfdc0a5
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-04-14 03:20:55 +0200

    added dark and light theme functions

--- lib/core/themes/app_theme.dart
+++ lib/core/themes/app_theme.dart
@@ -3,7 +3,10 @@ import 'package:flutter/services.dart';
 
 import 'app_colors.dart';
 
-ThemeData appTheme() => ThemeData(
-
+ThemeData lightAppTheme(context) => ThemeData(
       scaffoldBackgroundColor: AppColors.white,
+    );
+
+ThemeData darkAppTheme(context) => ThemeData(
+      scaffoldBackgroundColor: AppColors.darkGray,
     );
