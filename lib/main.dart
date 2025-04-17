37293ba594843783068b44122e3315fc31fa4ca8
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-04-17 07:34:34 +0200

    changed language to english

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,12 +14,14 @@ void main() async {
   await ScreenUtil.ensureScreenSize();
   setupGetIt();
   Bloc.observer = MyBlocObserver();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(
     EasyLocalization(
       supportedLocales: AppLocalizations.supportedLocales,
-      fallbackLocale: AppLocalizations.arabic,
+      fallbackLocale: AppLocalizations.english,
       path: AppLocalizations.translationsPath,
-      startLocale: AppLocalizations.arabic,
+      startLocale: AppLocalizations.english,
       saveLocale: true,
       child: MyApp(
         appRouter: AppRouter(),
