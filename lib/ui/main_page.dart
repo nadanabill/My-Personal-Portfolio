d39cf14979bc0325b4ed38809b3757c194be310a
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-04-17 07:36:56 +0200

    added web layout

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import '/ui/web/web_layout.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth < 1000) {
+              return const SizedBox();
+            } else {
+              return const WebLayout();
+            }
+          },
+        ),
+      ),
+    );
   }
 }
