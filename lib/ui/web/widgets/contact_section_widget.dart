197e4a7b7530509c393d438ff903e5258ea520e4
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-04-22 06:49:19 +0200

    added contact section widget

---
+++ lib/ui/web/widgets/contact_section_widget.dart
@@ -0,0 +1,67 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import '../../../core/helpers/spaces.dart';
+import '../../../core/themes/app_colors.dart';
+import '../../../core/themes/app_text_styles.dart';
+import '../../../core/utils/utils.dart';
+import '../../widgets/social_icons_widget.dart';
+import 'title_box_widget.dart';
+
+class ContactSectionWidget extends StatelessWidget {
+  const ContactSectionWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: AppColors.darkGray50,
+      padding: const EdgeInsets.symmetric(horizontal: 140, vertical: 60),
+      child: Column(
+        children: [
+          TitleBoxWidget(text: 'Get in touch'.tr()),
+          verticalSpace(15),
+          Text(
+            'What’s next? Feel free to reach out to me if you\'re looking for a developer, have a query, or simply want to connect.'
+                .tr(),
+            textAlign: TextAlign.center,
+            style: AppTextStyles.font20NormalDarkGray600(context),
+          ),
+          verticalSpace(30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.email_outlined,
+                size: 35,
+              ),
+              horizontalSpace(15),
+              Text(
+                'nadanabilhelmyy@gmail.com',
+                style: AppTextStyles.font30SemiBoldDarkGray900(context),
+              ),
+              horizontalSpace(15),
+              IconButton(
+                onPressed: () {
+                  copyToClipboard(
+                    context,
+                    'nadanabilhelmyy@gmail.com',
+                  );
+                },
+                icon: const Icon(Icons.copy_rounded, size: 35),
+              )
+            ],
+          ),
+          verticalSpace(20),
+          Text(
+            'You may also find me on these platforms!'.tr(),
+            textAlign: TextAlign.center,
+            style: AppTextStyles.font20NormalDarkGray600(context),
+          ),
+          verticalSpace(30),
+          const SocialIconsWidget(),
+        ],
+      ),
+    );
+  }
+}
