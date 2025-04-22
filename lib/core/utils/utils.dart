f2aec12226bda54de14adb7b8af3a08c05b8181f
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-04-22 06:49:00 +0200

    added copy to clipboard function

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,3 +1,6 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchUrlFunction(String link) async {
@@ -6,4 +9,14 @@ Future<void> launchUrlFunction(String link) async {
   if (!await launchUrl(url)) {
     throw Exception('Could not launch $url');
   }
+}
+
+void copyToClipboard(BuildContext context, String text) {
+  Clipboard.setData(ClipboardData(text: text)).then((_) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('${'Copied to clipboard:'.tr()}" $text"'),
+      ),
+    );
+  });
 }
