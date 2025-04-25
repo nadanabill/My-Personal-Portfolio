6a3e3e7a87c6f67c790a298bc9a0510e468e0760
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-04-25 11:32:52 +0300

    added user model

---
+++ lib/core/models/user_model.dart
@@ -0,0 +1,34 @@
+import 'experience_model.dart';
+import 'project_model.dart';
+import 'social_model.dart';
+import 'skills_model.dart';
+
+class UserModel {
+  final String name;
+  final String email;
+  final String logo;
+  final String photo;
+  final String profile;
+  final List<SocialIModel> socialLinks;
+  final String about;
+  final String profileSummary;
+  final String country;
+  final List<SkillsModel> skills;
+  final List<ExperienceModel> experiences;
+  final List<ProjectModel> projects;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.logo,
+    required this.photo,
+    required this.profile,
+    required this.socialLinks,
+    required this.about,
+    required this.profileSummary,
+    required this.country,
+    required this.skills,
+    required this.experiences,
+    required this.projects,
+  });
+}
