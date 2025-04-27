c9d988df00fdd1e5f2e2bb8ff53538f84602e189
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-04-27 05:59:17 +0300

    added mobile app bar widget

---
+++ lib/ui/mobile/widgets/mobile_app_bar_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/data/my_data.dart';
+import '../../../core/helpers/global_keys.dart';
+import '../../../core/helpers/spaces.dart';
+
+class MobileAppBarWidget extends StatelessWidget {
+  const MobileAppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Image.asset(
+            myData.logo,
+            height: 100,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {
+              GlobalKeys.mainKey.currentState!.openEndDrawer();
+            },
+            icon: const Icon(Icons.menu_rounded),
+          ),
+          horizontalSpace(10),
+        ],
+      ),
+    );
+  }
+}
