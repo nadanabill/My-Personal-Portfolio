d19203c56fefc009b23eeda63efa1d7f8e0ea075
Nada Nabil <nadanabilhelmyy@gmail.com>
2025-05-01 16:23:30 +0300

    update website files

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3089342992"
+    serviceWorkerVersion: "2941802955"
   }
 });
